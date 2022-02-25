b9815440a2fa737e5d5e39aa86bee18dc54666b9
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-02-25 22:05:52 +0000

    Music By Location - Part 1

--- MusicByLocation/ContentView.swift
+++ MusicByLocation/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var locationHandler = LocationHandler()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text(locationHandler.lastKnownLocation)
+                .padding()
+            Spacer()
+            Button("Find Music", action: {
+                locationHandler.requestLocation()
+            })
+        }.onAppear(perform: {
+            locationHandler.requestAuthorisation()
+        })
+        
     }
 }
 
---
+++ MusicByLocation/LocationHandler.swift
@@ -0,0 +1,46 @@
+//
+//  LocationHandler.swift
+//  MusicByLocation
+//
+//  Created by Nicholas Hodson on 25/02/2022.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate, ObservableObject {
+    let manager = CLLocationManager()
+    let geocoder = CLGeocoder()
+    @Published var lastKnownLocation: String = ""
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestAuthorisation() {
+        manager.requestWhenInUseAuthorization()
+    }
+    
+    func requestLocation() {
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let firstLocation = locations.first {
+            geocoder.reverseGeocodeLocation(firstLocation, completionHandler: {(placemarks, error) in
+                if error != nil {
+                    self.lastKnownLocation = "Could not perform lookup of location from coordinate information"
+                } else {
+                    if let firstPlacemark = placemarks?[0] {
+                        self.lastKnownLocation = firstPlacemark.locality ?? "Couldn't find locality"
+                    }
+                }
+            })
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        lastKnownLocation = "Error finding location"
+    }
+}
