2d0d37a0fc2f0c1914d1f23142845e577f49ec24
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-02-25 21:12:44 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Nicholas Hodson on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
